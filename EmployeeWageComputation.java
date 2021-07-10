d123432e7532c751255f5f5cbc7463fbaa8178ec
rohitdighe19 <rohitdighe47@gmail.com>
2021-07-10 23:10:52 +0530

    Solution to calculate daily employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,21 +1,28 @@
 /* Calculate Daily Employee Wage
- * Working Hour = 8
+ * Working Hour Full Time = 8
+ * Working Hour Part Time = 4
  * Wage Per Hour = 20
+
  */
 
 public class EmployeeWageComputation {
     public static void main(String args[]) {
         int empWagePerHour = 20;
         int empHour = 0;
-        int empPresent = 1;
-        int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
-        if (empCheck == empPresent) {
-            System.out.println("Employee is present");
+        int empPresentFullDay = 1;
+        int empAbsent = 0;
+        int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
+        if (empCheck == empPresentFullDay) {
+            System.out.println("Employee is present for full time");
             empHour = 8;
         }
-        else {
+        else if (empCheck == empAbsent) {
             System.out.println("Employee is absent");
             empHour = 0;
+        }
+        else {
+            System.out.println("Employee is present for part time");
+            empHour = 4;
         }
         wageCalculation(empWagePerHour, empHour);
     }
